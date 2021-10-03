eb3d9684ef352ea9b1376dceafa2caffddc7149e
brotherson67 <mitchell.brotherson@gmail.com>
2021-10-03 14:42:54 -0600

    made character arrays

--- Password_Generator/script.js
+++ Password_Generator/script.js
@@ -54,6 +54,13 @@ let numbersIncluded = function() {
     }
 };
 
+//array of characters
+//can merge arrays later?
+const lowerArray = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122];
+const upperArray = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
+const numberArray = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+const symbolArray = [33,34,35, 36, 37, 38, 39, 40, 52, 42, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96];
+//make master array, if characters are included or not
 
 
 // number generator 
