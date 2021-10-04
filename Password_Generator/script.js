07b0feaadf42b17ccab99f1daa06ee80ef6d3c8d
brotherson67 <mitchell.brotherson@gmail.com>
2021-10-03 20:13:28 -0600

    rearranged generatPassword function

--- Password_Generator/script.js
+++ Password_Generator/script.js
@@ -54,14 +54,6 @@ let numbersIncluded = function() {
     }
 };
 
-
-// passLength();
-// lowerIncluded();
-// upperIncluded();
-// symbolsIncluded();
-// numbersIncluded();
-
-
 //array of characters
 //can merge arrays later?
 const lowerArray = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122];
@@ -69,35 +61,30 @@ const upperArray = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
 const numberArray = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
 const symbolArray = [33,34,35, 36, 37, 38, 39, 40, 52, 42, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96];
 
-// lowerIncluded();
-
 //make master array, if characters are included or not
 // can merge master array by using [].concat(array) (I found this on https://dmitripavlutin.com/javascript-merge-arrays/)
-// let masterArray = [];
-// if (lowerIncluded) {masterArray = masterArray.concat(lowerArray)};
-// if (upperIncluded) {masterArray = masterArray.concat(upperArray)};
-// if (symbolsIncluded) {masterArray = masterArray.concat(symbolArray)};
-// if (numbersIncluded) {masterArray = masterArray.concat(numberArray)};
+let masterArray = [];
+if (lowerIncluded) {masterArray = masterArray.concat(lowerArray)};
+if (upperIncluded) {masterArray = masterArray.concat(upperArray)};
+if (symbolsIncluded) {masterArray = masterArray.concat(symbolArray)};
+if (numbersIncluded) {masterArray = masterArray.concat(numberArray)};
 
 // number generator 
 let charGenerator = function() {
-   const multipyNumber =  Math.floor(Math.random * masterArray.length);
-    const randCharacter = String.fromCharCode(multipyNumber)  //found this command on https://stackoverflow.com/questions/94037/convert-character-to-ascii-code-in-javascript
-    console.log(randCharacter);
+   //const multipyNumber =  Math.floor(Math.random * masterArray.length);
+   console.log(String.fromCharCode( Math.floor(Math.random * masterArray.length)))  //found this command on https://stackoverflow.com/questions/94037/convert-character-to-ascii-code-in-javascript
+
 };
 
-const generatePassword = function() {
-    passLength();
-    lowerIncluded();
-    upperIncluded();
-    symbolsIncluded();
-    numbersIncluded();
+charGenerator();
+
+const generatePassword = function(passLength, lowerIncluded, upperIncluded, symbolsIncluded, numbersIncluded ) {
+    let masterArray = [];
+    if (lowerIncluded) {masterArray = masterArray.concat(lowerArray)};
+    if (upperIncluded) {masterArray = masterArray.concat(upperArray)};
+    if (symbolsIncluded) {masterArray = masterArray.concat(symbolArray)};
+    if (numbersIncluded) {masterArray = masterArray.concat(numberArray)};
     for (let i = 0; i < passLength; i++){
-        let masterArray = [];
-            if (lowerIncluded) {masterArray = masterArray.concat(lowerArray)};
-            if (upperIncluded) {masterArray = masterArray.concat(upperArray)};
-            if (symbolsIncluded) {masterArray = masterArray.concat(symbolArray)};
-            if (numbersIncluded) {masterArray = masterArray.concat(numberArray)};
         let finalCharacter = charGenerator();    
         console.log(finalCharacter);
         const finalArray = [];
