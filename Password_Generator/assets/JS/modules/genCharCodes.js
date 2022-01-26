1c7c56e7b6bf3a578b69c2b1aae2c44e002b6bb6
brotherson67 <mitchell.brotherson@gmail.com>
2022-01-25 23:02:52 -0700

    created funciton to generate allowed characters

--- Password_Generator/assets/JS/modules/genCharCodes.js
+++ Password_Generator/assets/JS/modules/genCharCodes.js
@@ -1,10 +1,30 @@
-// I need to generate random characters that been select by checking the boxes
-// create a funciton that fills an array based on the selected buttons
+const includeLowercase = document.getElementById('lowerCase');
+const includeUppercase = document.getElementById('upperCase');
+const includeNumbers = document.getElementById('numbers');
+const includeSymbols = document.getElementById('symbols');
 
-const randomName = function(first, last) {
-    const allowedCharCode = [];
+
+// this function will generate an array of allowed characters 
+const genCharCodeArray = function(first, last) {
+    let allowedCharCode = [];
     for (let i = first; i <= last; i++){
         allowedCharCode.push(i);
     }
     return allowedCharCode;
 };
+
+// TESTS
+// let testVar = [];
+// testVar.push(...genCharCodeArray(97, 122));
+// console.log(testVar[0])
+// for each checkbox selected, a first and last character will be passed through. The array will be concatonated to another array
+
+const allowedCharCodeArray = function() {
+    let charCodeArray = [];
+    if (includeLowercase){
+        charCodeArray.push(...genCharCodeArray(97, 122))
+    };
+    if (includeUppercase){
+        charCodeArray.push(...genCharCodeArray(97, 122))
+    };
+}
