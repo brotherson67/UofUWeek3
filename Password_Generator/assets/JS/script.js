c93dbd43c933ba8abfcb74ef0f8a5f33cafb84ed
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-09 22:38:33 -0700

    changed properties

--- Password_Generator/assets/JS/script.js
+++ Password_Generator/assets/JS/script.js
@@ -1,8 +1,8 @@
-const includeLowercase = document.getElementById('lowerCase');
-const includeUppercase = document.getElementById('upperCase');
-const includeNumbers = document.getElementById('numbers');
-const includeSymbols = document.getElementById('symbols');
-const charNum = document.getElementById('charNumDiv');
+const includeLowercase = document.getElementById('lowerCase').checked;
+const includeUppercase = document.getElementById('upperCase').checked;
+const includeNumbers = document.getElementById('numbers').checked;
+const includeSymbols = document.getElementById('symbols').checked;
+const charNum = document.getElementById('charNumDiv').value;
 
 // ARRAYS 
 const lowerCaseLetters = ['a', 'b', "c", 'd', 'e', 'f', 'g', 'h', 'i', "j", 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -12,30 +12,34 @@ const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
 
-const allowedCharCodeArray = function() {
+const allowedCharCodeArray = function() {    // if an item is checked the corresponding characters are added to an array
     let charCodeArray = [];
     if (!includeSymbols.checked && !includeLowercase.checked && !includeNumbers.checked && !includeUppercase.checked){
         console.log("You didn't select anything. The function allowedCharCodeArray couldn't run right.")
     }
-    if (includeLowercase.checked){
+    if (includeLowercase){
         charCodeArray = charCodeArray.concat(...lowerCaseLetters)
     };
-    if (includeUppercase.checked){
+    if (includeUppercase){
         charCodeArray = charCodeArray.concat(...upperCaseLetters)
     };
-    if (includeNumbers.checked){
+    if (includeNumbers){
         charCodeArray = charCodeArray.concat(...numbers)
     };
-    if (includeSymbols.checked){
+    if (includeSymbols){
         charCodeArray = charCodeArray.concat(...symbols)
     };
     return charCodeArray
 };
 
-const charArray = allowedCharCodeArray();
-
 const generatePassword = () => {
+    const charArray = allowedCharCodeArray();
     const password = [];
+    for (let i = 0; i < charArray.length; i++) {    // this will add a random number to 
+        password.push(Math.floor(i * charNum))
+    }
+
+
     
     return password
 }
