77abbb66e5736233c9c14cc3732aec9fdb5670c3
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-10 19:11:58 -0700

    using generateBtn again

--- Password_Generator/assets/JS/script.js
+++ Password_Generator/assets/JS/script.js
@@ -2,7 +2,7 @@ const includeUppercase = document.getElementById('upperCase').checked;
 const includeNumbers = document.getElementById('numbers').checked;
 const includeSymbols = document.getElementById('symbols').checked;
 const charNum = document.getElementById('charNumDiv').value;
-const form = document.getElementById("form")
+
 
 // ARRAYS 
 const lowerCaseLetters = ['a', 'b', "c", 'd', 'e', 'f', 'g', 'h', 'i', "j", 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -14,29 +14,33 @@ const symbolsIncluded = includeSymbols.checked;
 
 function generatePassword(includeNumbers, includeSymbols, includeUppercase, charNum) {
     let charArray = lowerCaseLetters;
-    // if (includeUppercase.checked) {charArray = charArray.push(upperCaseLetters)};
+    if (includeUppercase) {charArray = charArray.push(upperCaseLetters)};
     if (includeNumbers) {charArray = charArray.push(numbers)};
-    // if (includeSymbols.checked) {charArray = charArray.concat(symbols)};
+    if (includeSymbols) {charArray = charArray.concat(symbols)};
     console.log(includeNumbers)
     return charArray
 }
 
-
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault()
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword(e) {
-    e.preventDefault()
+generateBtn.addEventListener("submit", (e) => {
+  e.preventDefault()
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-console.dir(includeNumbers)
+  console.dir(includeNumbers)
   passwordText.value = password;
-}
+});
+
+
+// Write password to the #password input
+// function writePassword(e) {
+//     e.preventDefault()
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+// console.dir(includeNumbers)
+//   passwordText.value = password;
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
