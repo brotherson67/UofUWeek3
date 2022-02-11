6fec36bc831f618385f3e0900f68dc3078a7c86f
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-10 21:13:47 -0700

    added comments and psuedo code

--- Password_Generator/assets/JS/script2.js
+++ Password_Generator/assets/JS/script2.js
@@ -10,6 +10,12 @@
     // add taht index to a new array
     // turn that new array into a string and return it
 
+const lowercase = document.getElementById('lowerCase');    // the lowercase checkbox element
+const upperCase = document.getElementById('upperCase');    // the uppercase checkbox element
+const numbers = document.getElementById('numbers');        // the numbers checkbox element
+const symbols = document.getElementById('symbols');        // the symbols checkbox element
+const charNum = document.getElementById('charNumDiv');     // the number of characters element
+const form = document.getElementById("form")               // this represents the entire form, may use for prevent default
 
 
 // Get references to the #generate element
